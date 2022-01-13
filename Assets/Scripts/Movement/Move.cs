5fd1b09b39b55a9e2f0a03724ee96b7810e2b83c
Szymon Rzosiński <szymonrzosinski@gmail.com>
2022-01-13 16:02:33 +0100

    Lepsze poruszanie

--- Assets/Scripts/Movement/Move.cs
+++ Assets/Scripts/Movement/Move.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
+using Valve.VR.InteractionSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class Move : MonoBehaviour
 {
     [SerializeField] SteamVR_Input_Sources sourceMovement, sourceTurning;
     [SerializeField] Transform leftHand;
-    [SerializeField] Transform head;
     [SerializeField] float speed = 5, rotationSpeed = 2;
-    [SerializeField] Rigidbody body;
+    CharacterController controller;
+    [SerializeField] LayerMask groundCheckLayerMask;
+    bool isGrounded = false;
 
     SteamVR_Action_Vector2 actionMovement, actionTurning;
 
@@ -18,31 +21,42 @@ public class Move : MonoBehaviour
     {
         actionMovement = SteamVR_Actions._default.Movement;
         actionTurning = SteamVR_Actions._default.Turning;
-        body = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
+
     private void FixedUpdate()
     {
-        Vector3 actPos = transform.position;
-        actPos.y = body.transform.position.y;
+        float distanceFromFloor = Vector3.Dot(Player.instance.hmdTransform.localPosition, Vector3.up);
+        controller.height = Mathf.Max(controller.radius, distanceFromFloor);
+        var newCenter = transform.InverseTransformPoint(Player.instance.hmdTransform.position);
+        newCenter.y = (controller.height / 2f);
+        controller.center = newCenter;
+        Vector2 moveVal = actionMovement.GetAxis(sourceMovement);
+        Vector3 forward = Vector3.ProjectOnPlane(leftHand.forward, Vector3.up);
+        Vector3 right = Vector3.ProjectOnPlane(leftHand.right, Vector3.up);
+        right = right.sqrMagnitude < 0.3f * 0.3f ? Vector3.zero : right.normalized;
+        forward = forward.sqrMagnitude < 0.3f * 0.3f ? Vector3.zero : forward.normalized;
+        if (moveVal.sqrMagnitude > 0.25f * 0.25f)
+        {
+            controller.Move((forward * moveVal.y * speed) + (right * moveVal.x * speed));
+        }
+        
+
+        Vector2 turnVal = actionTurning.GetAxis(sourceTurning);
+        transform.RotateAround(Player.instance.hmdTransform.position, Vector3.up, rotationSpeed * turnVal.x);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveVal = actionMovement.GetAxis(sourceMovement);
-        Vector3 forward = Vector3.ProjectOnPlane(leftHand.forward, Vector3.up);
-        Vector3 right = Vector3.ProjectOnPlane(leftHand.right, Vector3.up);
-        right = right.sqrMagnitude < 0.3f * 0.3f ? Vector3.zero : right.normalized;
-        forward = forward.sqrMagnitude < 0.3f * 0.3f?Vector3.zero: forward.normalized;
-        if (moveVal.sqrMagnitude > 0.25f * 0.25f)
+        if (Physics.BoxCast(transform.TransformPoint(controller.center) + Vector3.up * 0.1f, Vector3.one * 0.025f, Vector3.down, Quaternion.identity, 0.15f, groundCheckLayerMask))
         {
-            body.velocity = ((forward * moveVal.y * speed) + (right * moveVal.x * speed));
+            isGrounded = true;
         }
-        else if (body.velocity.x != 0 || body.velocity.z != 0)
-            body.velocity = new Vector3(0,body.velocity.y,0);
-
-
-        Vector2 turnVal = actionTurning.GetAxis(sourceTurning);
-        body.MoveRotation(body.rotation * Quaternion.AngleAxis(rotationSpeed * turnVal.x, Vector3.up));
+        else
+        {
+            isGrounded = false;
+            controller.Move(Vector3.down * 11f);
+        }
     }
 }
--- Assets/SteamVR/InteractionSystem/Core/Scripts/BodyCollider.cs
+++ Assets/SteamVR/InteractionSystem/Core/Scripts/BodyCollider.cs
@@ -8,22 +8,22 @@ public class BodyCollider : MonoBehaviour
 	[SerializeField] Transform head;
 	//[SerializeField] Rigidbody rb;
 
-	private CapsuleCollider capsuleCollider;
+	//private CapsuleCollider capsuleCollider;
 
 	void Awake()
 	{
-		capsuleCollider = GetComponent<CapsuleCollider>();
+		//capsuleCollider = GetComponent<CapsuleCollider>();
 		//rb = GetComponent<Rigidbody>();
 	}
 
 
 	void FixedUpdate()
 	{
-		float distanceFromFloor = Vector3.Dot( head.localPosition, Vector3.up );
+		/*float distanceFromFloor = Vector3.Dot( head.localPosition, Vector3.up );
 		capsuleCollider.height = Mathf.Max( capsuleCollider.radius, distanceFromFloor );
-		/*Vector3 headpos = head.localPosition - 0.5f * distanceFromFloor * Vector3.up;
-		headpos.y = rb.position.y;*/
-		capsuleCollider.center = Vector3.up * (capsuleCollider.height / 2f);
+		*//*Vector3 headpos = head.localPosition - 0.5f * distanceFromFloor * Vector3.up;
+		headpos.y = rb.position.y;*//*
+		capsuleCollider.center = Vector3.up * (capsuleCollider.height / 2f);*/
 		//rb.MovePosition(headpos);
 	}
 }
